8c2434f01cc65557ac8862f4b7107a728ad4bc40
Daniel Sosna <danik.sosna@gmail.com>
2025-11-10 16:22:44 +0200

    Extend ESLint config with React DOM rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactDom from 'eslint-plugin-react-dom'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -15,6 +16,7 @@ export default defineConfig([
       tseslint.configs.stylisticTypeChecked,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      reactDom.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
