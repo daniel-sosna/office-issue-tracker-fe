6474accd8e578433706217fe651a033acb957b18
Vitalijus Rusakevic <vitalijus.rusakevic@vandnesparkas.lt>
2025-11-13 03:34:28 +0200

    I’ve configured path aliases and updated my imports accordingly, so this issue is now resolved!

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import './index.css'
-import { AuthProvider } from './context/AuthContext'
-import { App } from './App'
+import { AuthProvider } from '@context/AuthContext'
+import { App } from '@app'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@pages': '/src/pages',
+      '@context': '/src/context',
+      '@app': '/src/App',
+    },
+  },
   server: {
     port: 5174,
     proxy: {
