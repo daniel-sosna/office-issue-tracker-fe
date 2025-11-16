d1697bdcc5ff4193c6680e37302693514f75be0a
Vitalijus Rusakevic <vitalijus.rusakevic@vandnesparkas.lt>
2025-11-16 13:27:09 +0200

    Refactoring is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Home } from '@pages/Home'
+import { Login } from '@pages/Login'
 
 const router = createBrowserRouter([
   {
