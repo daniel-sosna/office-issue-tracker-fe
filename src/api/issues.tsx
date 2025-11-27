e0a0cc3b7cfb44aaecad1504593747aadb0dc496
Daniel Sosna <danik.sosna@gmail.com>
2025-11-27 16:53:34 +0200

    Add api helper to fetch issue details

--- src/api/issues.tsx
+++ src/api/issues.tsx
@@ -1,3 +1,4 @@
+import { type Issue, type IssueDetails } from "@data/issues";
 import { csrfFetch } from "@utils/csrfFetch";
 
 export interface IssueData {
@@ -5,6 +6,33 @@ export interface IssueData {
   description: string;
   officeId: string;
 }
+
+interface IssueDetailsResponse {
+  issue: Issue;
+  office: string;
+  reportedBy: string;
+  reportedByAvatar: string;
+}
+
+export const fetchIssueDetails = async (
+  issueId: string
+): Promise<IssueDetails> => {
+  const res = await csrfFetch(
+    `http://localhost:8080/issues/${issueId}/details`
+  );
+
+  if (!res.ok) {
+    throw new Error(`HTTP ${res.status}`);
+  }
+  const data = (await res.json()) as IssueDetailsResponse;
+
+  return {
+    ...data.issue,
+    office: data.office,
+    reportedBy: data.reportedBy,
+    reportedByAvatar: data.reportedByAvatar,
+  };
+};
 
 export const createIssue = async (issue: IssueData): Promise<void> => {
   const res = await csrfFetch("http://localhost:8080/issues", {
