1d3be184024cd46b2aaf83f359d2ff923988d675
Edgar Miloš <edgarmilos7@gmail.com>
2025-11-28 01:24:36 +0200

    Add status converter from back-end to front-end values

---
+++ src/utils/issueStatusMapping.tsx
@@ -0,0 +1,8 @@
+export const backendToFrontendStatus: Record<string, string> = {
+  OPEN: "Open",
+  IN_PROGRESS: "In progress",
+  RESOLVED: "Resolved",
+  CLOSED: "Closed",
+  PENDING: "Pending",
+  BLOCKED: "Blocked",
+};
