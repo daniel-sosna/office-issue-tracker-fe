880595d1e3a7b52c95c846da9b880816c96bccd3
Edgar Miloš <edgarmilos7@gmail.com>
2025-11-28 01:45:43 +0200

    Add api logic to fetch paginated issues

--- src/api/issues.tsx
+++ src/api/issues.tsx
@@ -6,6 +6,22 @@ export interface IssueData {
   officeId: string;
 }
 
+export interface IssueDTO {
+  id: string;
+  summary: string;
+  description: string;
+  officeId: string;
+  status: string;
+}
+
+export interface PaginatedIssuesResponse {
+  content: IssueDTO[];
+  totalPages: number;
+  totalElements: number;
+  page: number;
+  size: number;
+}
+
 export const createIssue = async (issue: IssueData): Promise<void> => {
   const res = await csrfFetch("http://localhost:8080/issues", {
     method: "POST",
@@ -20,4 +36,19 @@ export const createIssue = async (issue: IssueData): Promise<void> => {
   }
 
   await res.json();
+};
+
+export const fetchIssues = async (
+  page = 1,
+  size = 10
+): Promise<PaginatedIssuesResponse> => {
+  const res = await csrfFetch(
+    `http://localhost:8080/issues?page=${page}&size=${size}`
+  );
+  if (!res.ok) {
+    throw new Error(`HTTP ${res.status}`);
+  }
+
+  const data = (await res.json()) as PaginatedIssuesResponse;
+  return data;
 };
