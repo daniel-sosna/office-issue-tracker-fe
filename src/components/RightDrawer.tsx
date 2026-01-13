ab910bd64004c47899d4388102763b79a6116db4
Daniel Sosna <danik.sosna@gmail.com>
2026-01-13 03:39:11 +0200

    Remove hidden buttons that won't be implemented

--- src/components/RightDrawer.tsx
+++ src/components/RightDrawer.tsx
@@ -1,8 +1,6 @@
 import { type ReactNode } from "react";
 import { Drawer, Box, IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import OpenInFullIcon from "@mui/icons-material/OpenInFull";
 
 interface Props {
   open: boolean;
@@ -26,23 +24,6 @@ export default function RightDrawer({ open, onClose, children }: Props) {
       }}
     >
       <Box display="flex" alignItems="center" justifyContent="end" gap={1}>
-        <IconButton
-          sx={{ display: "none" }}
-          size="small"
-          aria-label="expand"
-          title="Expand"
-        >
-          <OpenInFullIcon fontSize="small" color="secondary" />
-        </IconButton>
-
-        <IconButton
-          sx={{ display: "none" }}
-          size="small"
-          aria-label="more"
-          title="More options"
-        >
-          <MoreVertIcon fontSize="small" color="secondary" />
-        </IconButton>
         <IconButton
           onClick={onClose}
           size="small"
