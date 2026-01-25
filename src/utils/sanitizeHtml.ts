db953149da336041ca2fd24a87a84c585491bfd7
Edgar Miloš <edgarmilos7@gmail.com>
2026-01-26 01:56:25 +0200

    try to fix pipeline error

--- src/utils/sanitizeHtml.ts
+++ src/utils/sanitizeHtml.ts
@@ -3,5 +3,5 @@ import DOMPurify from "dompurify";
 export function sanitizeHtml(html: string): string {
   return DOMPurify.sanitize(html, {
     USE_PROFILES: { html: true },
-  });
+  }).toString();
 }
