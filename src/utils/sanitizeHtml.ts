8f659e5d93866e2703c74bfcc130572f4c6cfac7
Edgar Miloš <edgarmilos7@gmail.com>
2026-01-25 15:14:25 +0200

    try to fix pipeline error

--- src/utils/sanitizeHtml.ts
+++ src/utils/sanitizeHtml.ts
@@ -1,16 +1,7 @@
-import createDOMPurify from "dompurify";
-
-let DOMPurifyInstance: ReturnType<typeof createDOMPurify> | null = null;
+import DOMPurify from "dompurify";
 
 export function sanitizeHtml(html: string): string {
-  if (typeof window !== "undefined" && !DOMPurifyInstance) {
-    DOMPurifyInstance = createDOMPurify(window);
-  }
-
-  if (!DOMPurifyInstance) return "";
-
-  return DOMPurifyInstance.sanitize(html, {
-    ALLOWED_TAGS: ["p", "br", "strong", "em", "ul", "ol", "li", "a", "s"],
-    ALLOWED_ATTR: ["href", "target", "rel"],
+  return DOMPurify.sanitize(html, {
+    USE_PROFILES: { html: true },
   });
 }
